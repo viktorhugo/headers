d6c6ee59058a1daa05cda80b19034ba7ca48a16d
Glitch (synonymous-stupendous-hamster) <none>
2020-05-01 03:14:16 +0000

    🔔☎️ Updated with Glitch

---
+++ server.js
@@ -0,0 +1,30 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+
+// your first API endpoint... 
+app.get("/", function (req, res) {
+  return res.json({
+    ipaddress: req.headers["x-forwarded-for"].split(',')[0],
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  })
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
